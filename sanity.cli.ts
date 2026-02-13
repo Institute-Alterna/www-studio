341d487c59f2708888c39d366873d04bdb278593
Sebastián Camargo <sebastian@camargomar.in>
2026-02-12 19:38:59 -0500

    fix(cli): update environment variable names for project and app ID

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,8 +2,8 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_DATASET
   },
   deployment: {
     /**
@@ -11,6 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-    appId: process.env.SANITY_APP_ID
+    appId: process.env.SANITY_STUDIO_APP_ID
   }
 })
